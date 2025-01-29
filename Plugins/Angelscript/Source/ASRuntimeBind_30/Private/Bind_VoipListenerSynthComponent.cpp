7fd5869a4cd15c3736da59f9b250a5d3e8b4ac77
Will Gordon <wgordon9999@gmail.com>
2025-01-29 15:45:37 -0500

    Update Module 30

---
+++ Plugins/Angelscript/Source/ASRuntimeBind_30/Private/Bind_VoipListenerSynthComponent.cpp
@@ -0,0 +1,7 @@
+//#include "ASRuntimeBind_30Module.h"
+//#include "AngelscriptBinds.h"
+////#include "VoipListenerSynthComponent.h"
+//void FASRuntimeBind_30Module::Bind_VoipListenerSynthComponent()
+//{
+//			//FAngelscriptBinds::AddFunctionEntry(UVoipListenerSynthComponent::StaticClass(), "IsIdling", { ERASE_METHOD_PTR(UVoipListenerSynthComponent, IsIdling, (), ERASE_ARGUMENT_PACK(bool )) } );
+//}
